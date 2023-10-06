ef5443c5f92895bae1deabe614348705b6ada68c
Hiroshi Kera <kera@hiroshis-mbp.home>
2023-10-06 21:03:11 +0200

    load_dataを埋めた

--- src/cifar10.py
+++ src/cifar10.py
@@ -1,9 +1,7 @@
-import torch # 適宜importせよ
-
-
-'''
-Day1-2をもとに，load_data関数を完成させよ
-'''
+import torch.nn as nn 
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data import random_split, DataLoader
 
 def load_data(batch_size=128, n_train=15000, n_test=2500, use_all=False):
     '''
@@ -12,3 +10,26 @@ def load_data(batch_size=128, n_train=15000, n_test=2500, use_all=False):
     n_test    : テスト用のデータ数
     use_all   : 全てのデータを使う場合はTrueを与える
     '''
+    # クラスのラベル名
+    classes = ('airplane', 'automobile', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    ## 前処理関数の準備
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    # CIFAR10の準備（ローカルにデータがない場合はdataディレクトリにダウンロードされる）
+    # 訓練用データセット
+    trainset = CIFAR10(root='./data', train=True, download=True, transform=transform)
+    # 評価用データセット
+    testset = CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    if not use_all:
+        trainset, _ = random_split(trainset, [n_train, len(trainset) - n_train])  # trainsetの内，n_train個だけ選ぶ
+        testset, _ = random_split(testset, [n_test, len(testset) - n_test])       # testsetの内，n_test個だけ選ぶ
+
+    # ミニバッチに小分けしておく．これを後で使う
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    # ミニバッチに小分けしておく．これを後で使う
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return (trainloader, testloader, classes)
